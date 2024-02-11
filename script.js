c5c9e0133736db307ec40ef8be437c749289269e
MonikaCiesielska <150713260+MonikaCie@users.noreply.github.com>
2024-02-11 14:08:55 +0000

    date functionality

--- script.js
+++ script.js
@@ -14,3 +14,46 @@ function time() {
 time()
 
 console.log(new Date())
+
+// -----: Date :-----
+
+const todaysDate = document.getElementById('todaysDate');
+
+function date() {
+    let date = new Date();
+    let day = date.getDay();
+    let dayDate = date.getDate();
+    let monthDate = date.getMonth();
+    let yearDate = date.getFullYear();
+
+    const dayName = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday"
+    ];
+
+    const monthName = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December"
+    ];
+
+    const dateNow = document.createElement('p');
+    todaysDate.append(dateNow);
+    dateNow.innerText = `${dayName[day]}, ${dayDate} ${monthName[monthDate]} ${yearDate}`
+
+}
+date();
