535a1fc035b6be19c32bece7a7ea8815676d330d
MonikaCiesielska <150713260+MonikaCie@users.noreply.github.com>
2024-02-12 10:32:29 +0000

    clock & date updated; tasks started

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// -----: Clock :-----
+// -----: CLOCK :-----
 
 const clock = document.getElementById('clock');
 
@@ -6,22 +6,40 @@ function time() {
     let date = new Date();
     let hour = date.getHours();
     let minutes = date.getMinutes();
+    let minutesCorrected;
+    let hourCorrected;
 
-    if (minutes.length == 1) {
-        let minutesCorrected = `0${minutes}`;
+    if (minutes.toString().length == 1) {
+        minutesCorrected = `0${minutes}`;
     } else {
         minutesCorrected = minutes;
     }
 
+    if (hour.toString().length == 1) {
+        hourCorrected = `0${hour}`;
+    } else {
+        hourCorrected = hour;
+    }
+
     const timeNow = document.createElement('p');
     clock.append(timeNow);
-    timeNow.innerText = `${hour}:${minutesCorrected}`;
+    timeNow.innerText = `${hourCorrected}:${minutesCorrected}`;
+
+    timeNow.style.backgroundColor = '#2f3e46';
+    timeNow.style.width = '200px';
+    timeNow.style.fontFamily = '';
+    timeNow.style.fontSize = '3rem';
+    timeNow.style.borderRadius = '10px'
+    timeNow.style.padding = '10px'
+
 }
-time()
+
+setInterval(time(), 1000);
+
 
 console.log(new Date())
 
-// -----: Date :-----
+// -----: DATE :-----
 
 const todaysDate = document.getElementById('todaysDate');
 
@@ -62,4 +80,37 @@ function date() {
     dateNow.innerText = `${dayName[day]}, ${dayDate} ${monthName[monthDate]} ${yearDate}`
 
 }
-date();
+setInterval(date(), 1000)
+
+// -----: TO DO LIST :-----
+
+const inputToDo = document.getElementById('inputToDo');
+const addBtn = document.getElementById('addBtn');
+const toDoList = document.getElementById('toDoList');
+
+
+
+// addBtn.addEventListener('click', () => {
+//     let checkbox = document.createElement('input');
+//     checkbox.type = 'checkbox';
+//     checkbox.id = 'checkboxId'
+//     toDoList.append(checkbox);
+
+//     let label = document.createElement('label');
+//     label.htmlFor = 'checkboxId';
+//     label.innerText = inputToDo.value;
+//     toDoList.append(label)
+
+//     inputToDo.value = ""
+//     inputToDo.focus()
+
+// })
+
+addBtn.addEventListener('click', newItem())
+
+
+function newItem() {
+    let li = document.createElement('li');
+    let inputText = inputToDo.value;
+    console.log(inputText)
+}
